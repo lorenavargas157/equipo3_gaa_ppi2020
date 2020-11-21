8560085047420d4535a08dedfb0f445cfb4ae2b4
Santiago López Isaza <66447216+Santiago-30@users.noreply.github.com>
2020-11-20 19:15:28 -0500

    Actualización

---
+++ backend/routes/restaurantes.js
@@ -0,0 +1,10 @@
+const express = require('express');//tabNIne
+const router = express.Router();
+const mysqlConnection =require('../db/db');
+
+
+/// contenido
+
+module.exports = router;
+
+//crear los otros metodos, y estoy probando el repocitorio
--- backend/routes/usuario.js
+++ backend/routes/usuario.js
@@ -1,16 +1,69 @@
-const express = require('express');//tabNIne
-const router = express.Router();
-const mysqlConnection =require('../db/db');
+const {Router} = require('express');
+const usuario = usuario ();
+
+const mysqlConnection = require('./db/db.js');
+
+usuario.get('/',(req,res)=>{
+  res.send('Si funciona')
+})
+
+        // Tipo usuario
+    //Petición get
+    usuario.get('/usuario.js',(req,res)=>{
+    mysqlConnection.query('SELECT * FROM usuario.js',
+    (err,rows,fields)=>{
+      if(!err)
+     {
+       res.json(rows);
+     }else{
+       console.log(err);
+     }
+    })
+}) 
+
+    //Petición post
+    usuario.post('/usuario.js', (req, res) => {
+    const {nombre, descripcion} = req.body
+    let tipo = [nombre,descripcion];
+    let nuevoTipo = `INSERT INTO usuario.js VALUES (?,?);`
+
+    usuario.query(nuevoTipo,tipo, (err,results,fields) => {
+     if(err){
+       return console.error(err.message);
+     }
+     res.json({message:`usuario.js Ingresado`})
+   });
+  });
+  
+     //Petición put
+     usuario.put('/usuario.js/:id', (req,res) => {
+  const {nombre, descripcion} = req.body
+  const { id } = req.params 
+let tipo = [nombre, descripcion, id];
+let ActualizarTipo =`UPDATE usuario.js SET Nombre = ? Descripcion = ?  WHERE ID = ?`;
+mysqlConnection.query(ActualizarTipo,tipo, (err, rows,fields) => {
+   if(!err){
+    res.json({status: `usuario ha sido actualizado con éxito`});
+   }else{
+     console.log(err);
+   }
+});
+});
+
+  //PETICIÓN O SERVICIO DELETE - ELIMINACIÓN DE DATOS
+  usuario.delete('/usuario.js/:ID', (req,res) => {
+    const { id } = req.params;
+    mysqlConnection.query(`DELETE FROM usuario WHERE ID =?`,[id],(err,rows,fields) => {
+      if("!err"){
+        res.json({status: `El usuario se ha sido eliminado`})
+      }else{
+        console.log(err);
+      }
+    });
+  });
 
 
-/// contenido
-router.get('/usuario',(req,res)=>{
-mysqlConnection.query('SELECT * from usuario ',(err,rows,fiels)=>{
-if(!err){
-   res.json(rows); 
-}else{
-console.log(err);
-}});
-})// fin
+  
 
-module.exports = router;
+
+module.exports = usuario;
